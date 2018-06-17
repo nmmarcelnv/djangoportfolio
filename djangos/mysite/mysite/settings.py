d5353b13bfc27d482e7a3aafed992694f5e7545d
valery <nmmarcelnv@gmail.com>
2018-06-16 21:21:51 -0400

    reorganized statics

--- djangos/mysite/mysite/settings.py
+++ djangos/mysite/mysite/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+##or
+#STATIC_ROOT = '/home/nmmarcelnv/portfolio/djangos/mysite/static'
+
