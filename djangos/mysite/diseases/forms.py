436aed4da335d7f406d40c8cc798380cbe391e37
valery <nmmarcelnv@gmail.com>
2018-08-12 13:16:47 -0400

    unwanted

--- djangos/mysite/diseases/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Country
-
-class CountryForm(forms.Form):
-    #create tuples (country_code, country_name) 
-    countries = Country.objects.all()
-    CHOICES = ((country.country_code, country.country_name) for country in countries)
-    #post = forms.CharField()
-    country = forms.ChoiceField(widget=forms.Select, choices=CHOICES)
